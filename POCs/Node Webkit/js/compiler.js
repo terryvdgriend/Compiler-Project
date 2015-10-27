4a6e0082bb6648b3cb145c6e3e07eb34a8728fe6
Maurice van Breukelen <maurice@codematters.nl>
2015-10-27 23:54:11 +0100

    Kleine verbeteringen

--- POCs/Node Webkit/js/compiler.js
+++ POCs/Node Webkit/js/compiler.js
@@ -6,21 +6,23 @@ exports.run = function(code) {
 
 	var exec = require('child_process').exec;
 	var cmd = './compiler/' + compilerFile + ' -c "' + code + '"';
-	console.log(cmd);
 	
 	exec(cmd, function(error, stdout, stderr) {
-		if(error && stderr) {
-			var result = "ERROR!" + "<br>" + error + "<br><br>" + stderr;
-			console.log(result);
-			exports.setLogResult(result);
-		} else {
-			var resultWithBrs = stdout.replace(/(?:\r\n|\r|\n)/g, '<br/>');
-			var resultWithSpaces = resultWithBrs.replace(/ /g, '&nbsp;');
-			exports.setLogResult(resultWithSpaces);
+		var resultWithBrs = stdout.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+		var resultWithSpaces = resultWithBrs.replace(/ /g, '&nbsp;');
+		exports.setLogResult(resultWithSpaces);
+
+		if(error || stderr) {
+			var result = "<br><br>" + error + "<br><br>" + stderr;
+			exports.appendLogResult(result);
 		}
 	});
 };
 
-exports.setLogResult = function(result) {
-	$("#log div").html(result);
+exports.setLogResult = function(html) {
+	$("#log div").html(html);
+}
+
+exports.appendLogResult = function(html) {
+	$("#log div").html($("#log div").html() + html);
 }
