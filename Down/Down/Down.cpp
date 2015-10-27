e5027eb810829d6ac75e276609ee0848ca52fcf8
terryvdgriend <tgriend@student.avans.nl>
2015-10-27 22:25:22 +0100

    fixed main

--- Down/Down/Down.cpp
+++ Down/Down/Down.cpp
@@ -17,16 +17,41 @@
 #include "DoNothingNode.h"
 
 string getTextFromFile();
+string getTextFromFile(string filename);
 
-int main()
+int main(int argc, const char * argv[])
 {
+	string code = "";
+
+	if (argc == 3) {
+		string option = argv[1];
+		string value = argv[2];
+
+		if (option == "-f") {
+			// File
+			// std::cout << value << std::endl;
+			code = getTextFromFile(value);
+		}
+		else if (option == "-c") {
+			// Code
+			// std::cout << value << std::endl;
+			code = value;
+		}
+		else {
+			std::cout << "No valid option: " << option << std::endl;
+			return 0;
+		}
+	}
+	else {
+		std::cout << "Not enough params" << std::endl;
+		return 0;
+	}
 	//Declas
 	LinkedList cTokenList;
-	string codefromfile = getTextFromFile();//
 
 	//=========TOKENIZER==============
 	Tokenizer tnzr{ Tokenizer() };
-	tnzr.createTokenList(cTokenList, codefromfile);
+	tnzr.createTokenList(cTokenList, code);
 	tnzr.printTokenList(cTokenList);
 
 	//=========COMPILE CONDITION======
@@ -52,14 +77,13 @@ int main()
 		Program prog{ Program() };
 
 	}
-
-
-
-
-	//ZI ENDZ
-	int inputz = -1;
-	std::cin >> inputz;
 	return 0;
+}
+
+string getTextFromFile(string fileName) {
+	string datFile = "./" + fileName + ".md";
+	FileStreamer fs{ FileStreamer() };
+	return fs.reader(datFile);
 }
 
 
