fa835d24c302d42382f2ae79f06031ef667b54e7
Robin Collard <r-collard@outlook.com>
2015-10-28 17:13:47 +0100

    equals command

--- Down/Down/EqualsCommand.cpp
+++ Down/Down/EqualsCommand.cpp
@@ -6,4 +6,10 @@ void EqualsCommand::execute(VirtualMachine& vm, vector<string>& parameters)
 {
 	Variable variable1 = *vm.getVariable(parameters.at(1));
 	Variable variable2 = *vm.getVariable(parameters.at(2));
+
+	variable1 = variable2;
+
+	for each(string item in vm.getFunctionParametersByKey(parameters.at(1))) {
+		vm.setVariable(item, variable1.getValue());
+	}
 }
