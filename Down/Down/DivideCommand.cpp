bc74e3ad3a037cdb91f909c032686babeddadc40
Robin Collard <r-collard@outlook.com>
2015-10-29 11:34:31 +0100

    plus, times, minus, divide

--- Down/Down/DivideCommand.cpp
+++ Down/Down/DivideCommand.cpp
@@ -4,5 +4,22 @@
 
 void DivideCommand::execute(VirtualMachine& vm, vector<string>& parameters)
 {
-	//Todo
+	Variable variable1 = *vm.getVariable(parameters.at(1));
+	Variable variable2 = *vm.getVariable(parameters.at(2));
+
+	if (variable1.getType() == VariableType::NUMBER && variable2.getType() == VariableType::NUMBER) {
+
+		int number1 = atoi(variable1.getValue().c_str());
+		int number2 = atoi(variable2.getValue().c_str());
+
+		if (number2 != 0) {
+			vm.setReturnValue(to_string(number1 / number2));
+		}
+		else {
+			// Exception delen door 0
+		}
+	}
+	else {
+		// Exception delen heeft 2 nummers nodig
+	}
 }
