7ce34359a5e525959c91e42421939a016d3b5731
mikevanemmerik <mikevanemmerik14@hotmail.com>
2015-11-19 12:25:08 +0100

    dingen

--- Down/Down/CompileUserFunction.cpp
+++ Down/Down/CompileUserFunction.cpp
@@ -5,15 +5,39 @@
 
 CompileUserFunction::CompileUserFunction() 
 {
-	/*_compiledStatement = new LinkedActionList();
-	_condition = new LinkedActionList();
-	_body = new LinkedActionList();
-	_compiledStatement->add(new DoNothingNode());*/
+	_compiledStatement = new LinkedActionList();
+	//_condition = new LinkedActionList();
+	//_body = new LinkedActionList();
+	_compiledStatement->add(new DoNothingNode());
 }
 
 void CompileUserFunction::Compile(LinkedList & cTokenList, Token & begin, Token & end, LinkedActionList & listActionNodes, ActionNode & actionBefore)
 {
-	begin = *begin.getPartner();
+	//begin = *begin.getPartner();
+	Token* current = &begin;
+	int Level = begin.getLevel();
+	//
+	std::list<TokenExpectation> expected = std::list<TokenExpectation>();
+	expected.push_back(TokenExpectation(Level, Token::FUNCTION_OPEN));
+	expected.push_back(TokenExpectation(Level, Token::FUNCTION));
+	expected.push_back(TokenExpectation(Level + 1, Token::COMINGPARAMETER));
+	expected.push_back(TokenExpectation(Level, Token::CONDITION_CLOSE));
+	expected.push_back(TokenExpectation(Level, Token::BODY_OPEN));
+	expected.push_back(TokenExpectation(Level + 1, Token::ANY));
+	expected.push_back(TokenExpectation(Level, Token::FUNCTION_CLOSE));
+	for (TokenExpectation expectation : expected)
+	{
+		while (current->getEnum() == Token::NEWLINE) {
+			current = current->next;
+		}
+		if (current->getEnum() != expectation.TokenType) {
+			ErrorHandler::getInstance()->addError(Error{ "", ".md", current->getLevel(), current->getPositie(), Error::error }, expectation.TokenType, current->getEnum());
+			begin = end;
+			break;
+		}
+		else
+			current = current->next;
+	}
 }
 
 CompileUserFunction::~CompileUserFunction()
--- Down/Down/CompileUserFunction.h
+++ Down/Down/CompileUserFunction.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "CompileOperator.h"
+#include "DoNothingNode.h"
+#include "TokenExpectation.h"
 
 class CompileUserFunction :
 	public Compiler
 {
+private:
+	LinkedActionList* _compiledStatement;
+	//LinkedActionList* _condition;
+	LinkedActionList* _body;
 public:
 	CompileUserFunction();
 	void Compile(LinkedList& cTokenList, Token& begin, Token& end, LinkedActionList& listActionNodes, ActionNode& actionBefore);
