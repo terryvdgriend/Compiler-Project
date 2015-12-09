4b0f77f2daa448d04b28314024d162fcdad092bb
Maurice van Breukelen <maurice@codematters.nl>
2015-12-09 19:56:22 +0100

    Aangepaste regex

--- Down/Down/Tokenizer.h
+++ Down/Down/Tokenizer.h
@@ -23,8 +23,8 @@ private:
 	void checkRemainingErrors();
 	//Omdat else if als eerst staat zal deze gekozen worden..  nasty work around.
         
-    // (#+ (?:else if|else|if|case|while|do|foreach|for|\w+)|and gives|multiplied by|(^>.*\n)|(smaller|larger) than|^-?\d.?\d*$|"(.*?)"|\w+|\*\*\S*?\*\*|-{1,3}|^[\(\)\[\]]$|[\S|\n])
-	regex e{ "(#+ (?:else if|else|if|case|while|do|foreach|for|\\w+)|and gives|multiplied by|(^>.*\\n)|(smaller|larger) than|^-?\\d.?\\d*$|\"(.*?)\"|\\w+|\\*\\*\\S*?\\*\\*|-{1,3}|^[\\(\\)\\[\\]]$|[\\S|\\n])" };
+    // (\*{2}\S+?\*{2}|#+ (?:else if|\w+)|and gives|multiplied by|^>.*\n|(\w+) than|^-?\d*\.?\d*$|"(.*?)"|\w+|-{1,3}|[\S|\n])
+	regex e{ "(\\*{2}\\S+?\\*{2}|#+ (?:else if|\\w+)|and gives|multiplied by|^>.*\\n|(\\w+) than|^-?\\d*\\.?\\d*$|\"(.*?)\"|\\w+|-{1,3}|[\\S|\\n])" };
 	std::string lookAhead(smatch m, std::string s);
 public:
 	Tokenizer();
