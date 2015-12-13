d95e169a96c6b05ec457c25a4232e7b9c004611a
Mike <mikevanemmerik14@hotmail.com>
2015-12-13 14:00:57 +0100

    test voor: IfElse / Functions / Switch

--- Down/Down/TestText.h
+++ Down/Down/TestText.h
@@ -66,11 +66,57 @@ public:
 		--
 			[ ThrowError "2 can never be 1.." ]
 		__	
+		#### else if (3 like 1)	
+		--
+			[ ThrowError "3 can never be 1.." ]
+		__	
 		#### else
 		--
 			> Hier komt ie uit
 		__
-					 
+		
+	> Testing IFELSE with identifiers	
+    _number_ **i** is 6
+	#### if (**i** like **i**)	
+	--	
+        > we hebben geen not like
+	__
+	#### else
+	--
+		[ ThrowError "i should be equal to its own" ]
+	__
+	
+	_number_ **a** is 1
+	_number_ **b** is 2
+	_number_ **c** is **a** plus **b**
+	
+	#### if (**a** plus **b** like  **c**)	
+	--	
+        > Dat is correct
+	__
+	#### else
+	--
+		[ ThrowError "3 should be equal to 3" ]
+	__
+	
+	> IFELSE inside IF
+	#### if (1 like  1)	
+	--	
+        #### if (2 like  2)	
+    	--	
+            > Dat is correct
+    	__
+    	#### else
+    	--
+			> TODO: else gaat fout inside if, maar if inside if gaat wel goed
+    		[ ThrowError "should take if  1 == 1" ]
+		__
+	__
+	#### else
+	--
+		[ThrowError "should take if  1 == 1"]
+	__
+		 
 		)";
 									
 		return lolz;
@@ -79,7 +125,22 @@ public:
 	static const std::string GetFunctions()
 	{
 		auto lolz = R"(
-		_number_ **xx** is 2
+		---
+		### PlusOne gets _number_ **i** and gives _number_ **result** 
+		**i** is **i** plus 1
+		**result** is **i**
+		___
+
+		_number_ **retVal** is [ PlusOne 199 ]
+
+		#### if (**retVal** like 200)	
+		--	
+			> Correctico!
+		__	
+		#### else if (2 like 1)	
+		--
+			[ ThrowError "Output should be 200" ]
+		__	
 		 )";
 		return lolz;
 	}
