5c744fdc7baefc1ee5a601b97d5686ebdf9e673f
terryvdgriend <tgriend@student.avans.nl>
2015-12-16 10:17:59 +0100

    now possible to give an array as parameter to a function

--- Down/Down/EqualsCommand.cpp
+++ Down/Down/EqualsCommand.cpp
@@ -1,23 +1,29 @@
 #include "stdafx.h"
 #include "EqualsCommand.h"
 #include "MandatoryCommandIncludes.h"
+#include "CompileSingleStatement.h"
 
 void EqualsCommand::execute(VirtualMachine& vm, AbstractFunctionCall& node)
 {
 	vector<string>& parameters = node.getContentArrayNonConstant();
 
-	shared_ptr<Variable> variable1 = vm.getVariable(parameters.at(1));
+ 	shared_ptr<Variable> variable1 = vm.getVariable(parameters.at(1));
 	shared_ptr<Variable> variable2 = vm.getVariable(parameters.at(2));
 
 	if (variable1->getTokenType() == variable2->getTokenType()) 
 	{
-
-		variable1 = variable2;
-
-		for (string& item : vm.getFunctionParametersByKey(parameters.at(1))) 
-		{
-			vm.setVariable(item, variable1->getValue(), variable1->getTokenType());
+		if (variable1->getTokenType() == IToken::TYPE_FACT_ARRAY || variable1->getTokenType() == IToken::TYPE_NUMBER_ARRAY || variable1->getTokenType() == IToken::TYPE_TEXT_ARRAY) {
+			setArrayToArray(vm, parameters);
 		}
+		else {
+			variable1 = variable2;
+
+			for (string& item : vm.getFunctionParametersByKey(parameters.at(1)))
+			{
+				vm.setVariable(item, variable1->getValue(), variable1->getTokenType());
+			}
+		}
+		
 	}
 	else 
 	{
@@ -28,4 +34,34 @@ void EqualsCommand::execute(VirtualMachine& vm, AbstractFunctionCall& node)
 pair<string, string> EqualsCommand::accept(CommandVisitor& commandVisitor) 
 {
 	return commandVisitor.visit(*this);
+}
+
+void EqualsCommand::setArrayToArray(VirtualMachine& vm, vector<string>& parameters) {
+	//get both array's
+	auto toArray = vm.getVariableArray(parameters[1]);
+	auto fromArray = vm.getVariableArray(parameters[2]);
+
+	string buffer;
+	
+	string localVariable;
+
+	if (toArray.size() == 0) {
+		vm.addArrayToDictionary(parameters[1], fromArray.size());
+		for (size_t i = 0; i < fromArray.size(); i++)
+		{
+			CompileSingleStatement varGetter = CompileSingleStatement();
+			localVariable = varGetter.getNextLocalVariableName(buffer);
+			vm.setVariable(localVariable, fromArray.at(i)->getValue(), fromArray.at(i)->getTokenType());
+			vm.addItemToVariableArrayAt(parameters[1], to_string(i), vm.getVariable(localVariable));
+		}
+		// create Array
+		//set old array to this array
+	}
+	else if(toArray.size() >= fromArray.size()) {
+		//set old array to this array
+	}
+	else {
+		// throw error index out of bounds
+	}
+
 }
