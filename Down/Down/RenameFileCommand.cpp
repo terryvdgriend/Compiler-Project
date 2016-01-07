52623c9a8d459156b61dc6e849680d42c33d5784
Wouter <wmaarts@student.avans.nl>
2016-01-07 11:50:49 +0100

    Cleanup + error handling improved

--- Down/Down/RenameFileCommand.cpp
+++ Down/Down/RenameFileCommand.cpp
@@ -4,6 +4,12 @@
 #include <errno.h>
 #include <stdio.h>
 
+#ifdef _WIN32
+#include "dirent.h"
+#else
+#include <dirent.h>
+#endif
+
 
 RenameFileCommand::RenameFileCommand()
 {
@@ -18,32 +24,40 @@ void RenameFileCommand::execute(VirtualMachine & vm, AbstractFunctionCall & node
 	vector<string>& parameters = node.getContentArrayNonConstant();
 	auto variable1 = vm.getVariable(parameters[1]);
 	auto variable2 = vm.getVariable(parameters[2]);
-	string oldFile = vm.getVariable(parameters[1])->getValue();
-	string newFile = vm.getVariable(parameters[2])->getValue();
-	if (getExtension(oldFile) != getExtension(newFile)) {
-		throwCustomError("Input does not have matching extensions", vm);
-		vm.triggerRunFailure();
-	}
+	if (variable1 != nullptr) {
+		if (variable2 != nullptr) {
+			if (variable1->getTokenType() == IToken::TYPE_TEXT && variable2->getTokenType() == IToken::TYPE_TEXT) {
+				string oldFile = vm.getVariable(parameters[1])->getValue();
+				string newFile = vm.getVariable(parameters[2])->getValue();
+				if (getExtension(oldFile) != getExtension(newFile)) {
+					throwCustomError("Input does not have matching extensions", vm);
+				}
+				if (opendir(oldFile.c_str()) == nullptr) {
+					throwCustomError("Cannot rename directories", vm);
+				}
 
-	oldFile.erase(remove(oldFile.begin(), oldFile.end(), '\"'), oldFile.end());
-	newFile.erase(remove(newFile.begin(), newFile.end(), '\"'), newFile.end());
-	int result = rename(oldFile.c_str(), newFile.c_str());
-	if (result == 0) {
-		cout << "File " << oldFile << " renamed to " << newFile << endl;
+				oldFile.erase(remove(oldFile.begin(), oldFile.end(), '\"'), oldFile.end());
+				newFile.erase(remove(newFile.begin(), newFile.end(), '\"'), newFile.end());
+				int result = rename(oldFile.c_str(), newFile.c_str());
+				if (result != 0) {
+					cout << "Error renaming file! Code: " << result << endl;
+					char buff[256];
+
+					#ifdef _WIN32
+						strerror_s(buff, 100, errno);
+					#else
+						strerror_r(100, buff, errno);
+					#endif
+
+					throwCustomError(buff, vm);
+				}
+			}
+			else {
+				throwCustomError("Parameters must be of type 'text'", vm);
+			}
+		}
 	}
-	else {
-		cout << "Error renaming file! Code: " << result << endl;
-		char buff[256];
-        
-        #ifdef _WIN32
-            strerror_s(buff, 100, errno);
-        #else
-            strerror_r(100, buff, errno);
-        #endif
-        
-		throwCustomError(buff, vm);
-		vm.triggerRunFailure();
-	}
+	throwCustomError("Parameters not set", vm);
 }
 
 string RenameFileCommand::getExtension(const string filename)
