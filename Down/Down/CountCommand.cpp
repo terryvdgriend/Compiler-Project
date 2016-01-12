a46bf2af49c0aaa149cc9ad144173cfeb32d3d2b
terryvdgriend <tgriend@student.avans.nl>
2016-01-12 12:51:59 +0100

    count function extended with array dimensions

--- Down/Down/CountCommand.cpp
+++ Down/Down/CountCommand.cpp
@@ -20,8 +20,36 @@ void CountCommand::execute(VirtualMachine & vm, AbstractFunctionCall & node)
 		return;
 	}
 	auto array =  vm.getVariableArray(parameters[1]);
-	vm.setReturnValue(to_string(array->variableArrayDictionary.size()));
-	vm.setReturnToken(IToken::TYPE_NUMBER);
+	if (array != nullptr) {
+		if (parameters.size() > 2) {
+			auto var = vm.getVariable(parameters[2]);
+			if (var->getTokenType() == IToken::TYPE_NUMBER && var->getType() == VariableType::number) {
+				int index = atof(var->getValue().c_str());
+				if (index < 0) {
+					throwCustomError("index is below zero.", vm);
+				}
+				else if (index > array->arraySizes.size() - 1) {
+					throwCustomError("index out of bounds range.", vm);
+				}
+				else {
+					vm.setReturnValue(to_string(array->arraySizes.at(index)));
+					vm.setReturnToken(IToken::TYPE_NUMBER);
+				}
+			}
+			else {
+				throwCustomError("input is not a number.", vm);
+			}
+		}
+		else {
+			vm.setReturnValue(to_string(array->variableArrayDictionary.size()));
+			vm.setReturnToken(IToken::TYPE_NUMBER);
+		}
+	}
+	else {
+		throwCustomError("array is not found.", vm);
+	}
+	
+
 }
 
 pair<string, string> CountCommand::accept(CommandVisitor & cmdVisitor)
